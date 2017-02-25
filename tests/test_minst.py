d7346cb7213945bdf77373af2f6fae144f994332
sudipta <basaks@gmail.com>
2017-02-25 17:54:45 +1100

    added mnist realval test

--- tests/test_minst.py
+++ tests/test_minst.py
@@ -5,10 +5,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 from sklearn.neural_network import BernoulliRBM
+from scipy.ndimage import convolve
 
 from emf_rbm import EMF_RBM
+from rbm_datasets import load_mnist_realval
 
-from scipy.ndimage import convolve
 np.seterr(all='warn')
 
 
@@ -57,32 +58,42 @@ def minst():
     return X_train, X_test, Y_train, Y_test
 
 
-@pytest.fixture(params=[1000, 5000])
+@pytest.fixture(params=[5000])
 def regulariser_C(request):
     return request.param
 
 
 def test_digits(regulariser_C, minst):
-    print('testing minst for C:', regulariser_C)
+    print('testing digists dataset')
     X_train, X_test, Y_train, Y_test = minst
-    B_rbm = BernoulliRBM(verbose=True, n_components=100, n_iter=50,
-                         learning_rate=0.06)
+    common_check(X_test, X_train, Y_test, Y_train, regulariser_C)
 
+
+def common_check(X_test, X_train, Y_test, Y_train, regulariser_C):
+    B_rbm = BernoulliRBM(verbose=True, n_components=100, n_iter=40,
+                         learning_rate=0.06)
     logistic = linear_model.LogisticRegression()
     logistic.C = regulariser_C
-
     classifier = Pipeline(steps=[('rbm', B_rbm), ('logistic', logistic)])
     classifier.fit(X_train, Y_train)
-
     Y_test_berboulli_pred = classifier.predict(X_test)
-
-    emf_rbm = EMF_RBM(verbose=True, n_components=100, n_iter=50,
+    emf_rbm = EMF_RBM(verbose=True, n_components=100, n_iter=40,
                       learning_rate=0.06)
-
     classifier = Pipeline(steps=[('rbm', emf_rbm), ('logistic', logistic)])
     classifier.fit(X_train, Y_train)
     Y_test_emf_pred = classifier.predict(X_test)
     emf_accuracy = accuracy_score(y_pred=Y_test_emf_pred, y_true=Y_test)
     bernoulli_accuracy = accuracy_score(y_pred=Y_test_berboulli_pred,
                                         y_true=Y_test)
+    print("bernoulli_accuracy:", bernoulli_accuracy)
+    print("emf_accuracy:", emf_accuracy)
     assert abs(emf_accuracy - bernoulli_accuracy) < 0.1
+
+
+def test_mnist_realval():
+    x_train, targets_train, x_valid, targets_valid, x_test, targets_test = \
+        load_mnist_realval()
+    X = np.vstack((x_train, x_test))
+    x_train = (x_train - np.min(X, 0)) / (np.max(X, 0) + 0.0001)  # 0-1 scaling
+    x_test = (x_test - np.min(X, 0)) / (np.max(X, 0) + 0.0001)  # 0-1 scaling
+    common_check(x_test, x_train, targets_test, targets_train, 5000)
